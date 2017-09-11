1be2957886c352e1b1753521324cfbaaf59732a6
shimil <shimil@wix.com>
2017-09-11 11:28:21 +0300

    splitting remoteDOM into local and remote bundles since they are usually loaded separately + adding debug bundles

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,60 @@
 const path = require('path');
 const webpack = require('webpack');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: "bundle.js",
-    libraryTarget: 'umd',
-    library: 'remoteDOM'
+module.exports = [
+  {
+    entry: {
+      local: './src/local.js',
+      remote: './src/remote.js',
+      bundle: './src/index.js'
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].min.js",
+      libraryTarget: 'umd',
+      library: '[name]DOM'
+    },
+    devtool: 'source-map',
+    module: {
+      loaders: [{
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+      ]
+    },
+    plugins: [new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      sourceMap: true
+    })]
   },
-  devtool: 'source-map',
-  module: {
-    loaders: [ {test: /\.js$/,
-      exclude: /(node_modules|bower_components)/,
-      loader: 'babel-loader',
-      query: {
-        presets: ['es2015']
-      }}
-    ]
-  },
-  plugins: [new webpack.optimize.UglifyJsPlugin({
-    minimize: true,
-    sourceMap: true
-  })]
-};
+  {
+    entry: {
+      local: './src/local.js',
+      remote: './src/remote.js',
+      bundle: './src/index.js'
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].js",
+      libraryTarget: 'umd',
+      library: '[name]DOM'
+    },
+    devtool: 'source-map',
+    module: {
+      loaders: [{
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+      ]
+    },
+    plugins: []
+  }
+];
