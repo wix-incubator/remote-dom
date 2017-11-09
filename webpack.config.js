9c356cd9e9915dea519c649f70b40cba3daa2e41
shimil <shimil@wix.com>
2017-11-09 10:10:14 +0200

    remove unused bundle that exposes both local and remote + exclude test files from coverage

--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,31 @@
 const path = require('path');
 const webpack = require('webpack');
 
-module.exports = [
-  {
-    entry: {
-      local: './src/local.js',
-      remote: './src/remote.js',
-      bundle: './src/index.js'
-    },
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: "[name].min.js",
-      libraryTarget: 'umd',
-      library: '[name]DOM'
-    },
-    devtool: 'source-map',
-    module: {
-      loaders: [{
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015']
-        }
-      }
-      ]
-    },
-    plugins: [new webpack.optimize.UglifyJsPlugin({
-      minimize: true,
-      sourceMap: true
-    })]
+module.exports = {
+  entry: {
+    local: './src/local.js',
+    remote: './src/remote.js',
   },
-  {
-    entry: {
-      local: './src/local.js',
-      remote: './src/remote.js',
-      bundle: './src/index.js'
-    },
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: "[name].js",
-      libraryTarget: 'umd',
-      library: '[name]DOM'
-    },
-    devtool: 'source-map',
-    module: {
-      loaders: [{
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015']
-        }
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].min.js",
+    libraryTarget: 'umd',
+    library: '[name]DOM'
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015']
       }
-      ]
-    },
-    plugins: []
-  }
-];
+    }
+    ]
+  },
+  plugins: [new webpack.optimize.UglifyJsPlugin({
+    minimize: true,
+    sourceMap: true
+  })]
+};
